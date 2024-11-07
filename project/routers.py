5167de4403ca74083ded78afcb33a165d152f920
mateosvilasboas <mateuscamposdantas@gmail.com>
2024-11-07 18:47:23 -0300

    feat: endpoints desenvolvidas

--- project/routers.py
+++ project/routers.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, Response, status
+from fastapi import APIRouter, Depends, Response, status, HTTPException
 from pydantic import BaseModel
 from sqlalchemy import not_, select, update
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -26,26 +26,35 @@ async def get_competitions(response: Response, db: AsyncSession = Depends(get_db
       competitions = obj.scalars().all()
 
       if not competitions:
-         response.status_code = status.HTTP_204_NO_CONTENT
-         return 
+         response.status_code = status.HTTP_404_NOT_FOUND
+         raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Not Found"
+         )
       
       response.status_code = status.HTTP_200_OK
-      body = {"competitions": competitions,
-            "status_code": response.status_code}
-      
-      return body
-   except:
-      response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-      body = {"status_code": response.status_code}
-      return body
+      response.body = {
+         "competitions": competitions,
+      }
+      return response.body
+   except Exception as e:
+      response.body = {"error": e}
+      return response.body
 
 @router.get("/get-ranking/{name}")
-async def get_ranking(name: str, db: AsyncSession = Depends(get_db)):
+async def get_ranking(response: Response, name: str, db: AsyncSession = Depends(get_db)):
    try:
       obj = await db.execute(select(Competition).where(Competition.name==name))
       competition = obj.scalars().all()
-      results = competition[0].__dict__["results"]
 
+      if not competition:
+         response.status_code = status.HTTP_404_NOT_FOUND
+         raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Not Found"
+         )
+
+      results = competition[0].__dict__["results"]
       ranking = []
       aux = []
 
@@ -63,62 +72,101 @@ async def get_ranking(name: str, db: AsyncSession = Depends(get_db)):
                ranking.append(result)
                aux.append(now_best)
  
-      return {"ranking": ranking}
+      response.status_code = status.HTTP_200_OK
+      response.body = {
+         "ranking": ranking
+      }
+
+      return response.body
    except Exception as e:
-      print(e)
-   
+      response.body = {"error": e}
+      return response.body
+
+@router.put("/change-competition-status")
+async def change_competition_status(response: Response, competition: CompetitionSchemaBase, db: AsyncSession = Depends(get_db)):
+   try:
+      obj = await db.execute(select(Competition).where(Competition.name==competition.name))
+      competitions = obj.scalars().all()
+
+      if not competitions:
+         response.status_code = status.HTTP_404_NOT_FOUND
+         raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Not Found"
+         )
+
+      await db.execute(update(Competition).where(Competition.name==competition.name).values(is_finished=not competitions[0].__dict__["is_finished"]))
+      await db.commit()
+      await db.refresh(competitions[0])
+      
+      response.status_code = status.HTTP_200_OK
+      response.body = {
+         "competition": competitions[0]
+      }
+      
+      return response.body
+   except Exception as e:
+      response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+      response.body = {"error": e}
+      return response.body
+
 @router.post("/create-competition")
 async def create_competition(response: Response, competition: CreateCompetitionSchema, db: AsyncSession = Depends(get_db)):
    try:   
       new_competition = Competition(name=competition.name, unit=competition.unit.lower())
+      print(new_competition)
+
       db.add(new_competition)
       await db.commit()
       await db.refresh(new_competition)
       
       response.status_code = status.HTTP_201_CREATED
-      body = {"competition": new_competition,
-            "status_code": response.status_code}
-
-      return body
-   except:
-      response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-      body = {"status_code": response.status_code}
-      return body
-
-@router.put("/change-competition-status")
-async def change_competition_status(name: str, db: AsyncSession = Depends(get_db)):
-   try:
-      obj = await db.execute(select(Competition).where(Competition.name==name))
-      competition = obj.scalars().first()
-      await db.execute(update(Competition).where(Competition.name==name).values(is_finished=not competition.is_finished))
-      await db.commit()
-      await db.refresh(competition)
-      
-      return competition
+      response.body = {
+         "competition": new_competition,
+      }
+      return response.body
+   except ValueError as e:
+      response.status_code = status.HTTP_409_CONFLICT
+      response.body = {"error": e.args}
+      return response.body
    except Exception as e:
-      print(e)
+      response.body = {"error": e}
+      return response.body
 
 @router.post("/create-result")
 async def create_result(response: Response, result: ResultSchemaBase, db: AsyncSession = Depends(get_db)):
    try:
       obj = await db.execute(select(Competition).where(Competition.name == result.competition))
-      competition = obj.scalars().first()
+      competition = obj.scalars().all()
 
-      if competition.is_finished:
-         response.status_code = status.HT
-         return {
-            "error": "Can't add result to finished competition."
+      if not competition:
+         response.status_code = status.HTTP_404_NOT_FOUND
+         raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Not Found"
+         )
+
+      if competition[0].__dict__["is_finished"]:
+         response.status_code = status.HTTP_409_CONFLICT
+         response.body = {
+            "error": "Competition is already finished!",
          }
+         return response.body
+
       new_result = Result(
          name=result.athlete,
          value=result.value,
          competition_id=competition.id
       )
-      if competition.is_finished:
-         raise ValueError("Can't create result, competition is finished!")
       db.add(new_result)
       await db.commit()
       await db.refresh(new_result)
-      return result
+
+      response.status_code = status.HTTP_200_OK
+      response.body = {
+         "result": new_result,
+      }
+      return response.body
    except Exception as e:
-      print(e)
+      response.body = {"error": e}
+      return response.body

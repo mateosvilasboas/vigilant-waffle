2ebd5aa9ea7b0c4e5aa2d812cea4bc88f2276851
mateosvilasboas <mateuscamposdantas@gmail.com>
2024-11-11 09:16:25 -0400

    style: indentaçao de codigo

--- project/routers.py
+++ project/routers.py
@@ -31,29 +31,34 @@ class AthleteSchemaBase(BaseModel):
 @router.get("/get-competitions", status_code=status.HTTP_200_OK)
 async def get_competitions(response: Response, db: AsyncSession = Depends(get_db)):
    try:
-      obj = await db.execute(select(Competition).order_by(Competition.id))
+      obj = await db.execute(select(Competition).
+                             order_by(Competition.id))
       competitions = obj.scalars().all()
 
       if not competitions:
          return JSONResponse(
-            format_error(status_code=status.HTTP_404_NOT_FOUND, error="competitions not found"),
+            format_error(status_code=status.HTTP_404_NOT_FOUND, 
+                         error="competitions not found"),
             status_code=status.HTTP_404_NOT_FOUND)
          
       return {"competitions": competitions}
    except Exception as e:
          return JSONResponse(
-            format_error(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, error=str(e)),
+            format_error(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, 
+                         error=str(e)),
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 @router.get("/get-ranking/{name}", status_code=status.HTTP_200_OK)
 async def get_ranking(response: Response, name: str, db: AsyncSession = Depends(get_db)):
    try:
-      obj = await db.execute(select(Competition).where(Competition.name==name.lower()))
+      obj = await db.execute(select(Competition).
+                             where(Competition.name==name.lower().strip()))
       competition = obj.scalars().all()
 
       if not competition:
          return JSONResponse(
-            format_error(status_code=status.HTTP_404_NOT_FOUND, error="competition not found"),
+            format_error(status_code=status.HTTP_404_NOT_FOUND, 
+                         error="competition not found"),
             status_code=status.HTTP_404_NOT_FOUND)
 
       athletes = competition[0].__dict__["athletes"]
@@ -90,22 +95,26 @@ async def get_ranking(response: Response, name: str, db: AsyncSession = Depends(
       }
    except Exception as e:
       return JSONResponse(
-         format_error(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, error=str(e)),
+         format_error(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, 
+                      error=str(e)),
          status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 @router.put("/change-competition-status", status_code=status.HTTP_200_OK)
 async def change_competition_status(response: Response, body: CompetitionUpdateSchemaBase, db: AsyncSession = Depends(get_db)):
    try:
-      obj = await db.execute(select(Competition).where(Competition.id==body.id))
+      obj = await db.execute(select(Competition).
+                             where(Competition.id==body.id))
       competition = obj.scalars().all()
 
       if not competition:
          return JSONResponse(
-            format_error(status_code=status.HTTP_404_NOT_FOUND, error="competition not found"),
+            format_error(status_code=status.HTTP_404_NOT_FOUND, 
+                         error="competition not found"),
             status_code=status.HTTP_404_NOT_FOUND)
 
-      await db.execute(update(Competition).where(Competition.id==competition[0].id
-                                                 ).values(is_finished=not competition[0].__dict__["is_finished"]))
+      await db.execute(update(Competition).
+                       where(Competition.id==competition[0].id).
+                       values(is_finished=not competition[0].__dict__["is_finished"]))
       await db.commit()
       await db.refresh(competition[0])
       
@@ -114,7 +123,8 @@ async def change_competition_status(response: Response, body: CompetitionUpdateS
       }
    except Exception as e:
       return JSONResponse(
-         format_error(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, error=str(e)),
+         format_error(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, 
+                      error=str(e)),
          status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 @router.post("/create-competition", status_code=status.HTTP_201_CREATED)
@@ -122,10 +132,13 @@ async def create_competition(response: Response, body: CreateCompetitionSchema, 
    try:
       if not body.name:
          return JSONResponse(
-            format_error(status_code=status.HTTP_400_BAD_REQUEST, error="competition must have a name"),
+            format_error(status_code=status.HTTP_400_BAD_REQUEST, 
+                         error="competition must have a name"),
             status_code=status.HTTP_400_BAD_REQUEST)
 
-      new_competition = Competition(name=body.name.lower(), unit=body.unit.lower(), number_of_attempts=body.number_of_attempts)
+      new_competition = Competition(name=body.name.lower().strip(),
+                                    unit=body.unit.lower().strip(), 
+                                    number_of_attempts=body.number_of_attempts)
 
       db.add(new_competition)
       await db.commit()
@@ -137,22 +150,26 @@ async def create_competition(response: Response, body: CreateCompetitionSchema, 
    
    except ValueError as e:
       return JSONResponse(
-         format_error(status_code=status.HTTP_409_CONFLICT, error="only accepts 'meters' or 'seconds'"),
+         format_error(status_code=status.HTTP_409_CONFLICT, 
+                      error="only accepts 'meters' or 'seconds'"),
          status_code=status.HTTP_409_CONFLICT)
    except IntegrityError as e:
       return JSONResponse(
-         format_error(status_code=status.HTTP_409_CONFLICT, error="name must be unique"),
+         format_error(status_code=status.HTTP_409_CONFLICT, 
+                      error="name must be unique"),
          status_code=status.HTTP_409_CONFLICT)
    except Exception as e:
       return JSONResponse(
-         format_error(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, error=str(e)),
+         format_error(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, 
+                      error=str(e)),
          status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 @router.post("/create-result", status_code=status.HTTP_201_CREATED)
 async def create_result(response: Response, body: AthleteSchemaBase, db: AsyncSession = Depends(get_db)):
    try:
 
-      obj = await db.execute(select(Competition).where(Competition.name == body.competition.lower()))
+      obj = await db.execute(select(Competition).
+                             where(Competition.name == body.competition.lower().strip()))
       competition = obj.scalars().all()
       
       if not competition:
@@ -173,11 +190,12 @@ async def create_result(response: Response, body: AthleteSchemaBase, db: AsyncSe
 
       if not body.athlete:
          return JSONResponse(
-            format_error(status_code=status.HTTP_400_BAD_REQUEST, error="athlete must have a name"),
+            format_error(status_code=status.HTTP_400_BAD_REQUEST, 
+                         error="athlete must have a name"),
             status_code=status.HTTP_400_BAD_REQUEST)
 
       new_result = Athlete(
-         name=body.athlete,
+         name=body.athlete.strip(),
          competition_id=competition[0].id,
       )
 
@@ -195,5 +213,6 @@ async def create_result(response: Response, body: AthleteSchemaBase, db: AsyncSe
       return {"new_result": new_result}
    except Exception as e:
       return JSONResponse(
-         format_error(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, error=str(e)),
+         format_error(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, 
+                      error=str(e)),
          status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
